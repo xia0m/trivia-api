f1dbb81f451c67acdced92a4aa3385f84fa985b7
xia0m <alexma325@gmail.com>
2019-12-28 01:48:12 -0800

    feat: added 400,405,500 error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -254,13 +254,38 @@ def create_app(test_config=None):
 		Create error handlers for all expected errors
 		including 404 and 422.
 		'''
+
+    @app.errorhandler(400)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request'
+        }), 400
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'Method Not Allowed'
+        }), 405
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Internal Server Error'
+        }), 500
+
     @app.errorhandler(422)
     def unprocessable(error):
-            return jsonify({
-                'success': False,
-                'error': 422,
-                'message': 'unprocessable'
-            }), 422
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable'
+        }), 422
 
     @app.errorhandler(404)
     def not_found(error):
