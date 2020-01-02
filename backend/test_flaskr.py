79dc163affcee7a815e6d1ea963acf7b59c003e5
xia0m <alexma325@gmail.com>
2020-01-02 01:51:26 -0800

    feat: added testing for create a new question

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -99,55 +99,23 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'unprocessable')
 
-    # def test_create_new_questions(self):
-    #     res = self.client().post('/api/questions', json=self.new_question)
-    #     data = json.loads(res.data)
+    def test_create_new_questions(self):
+        res = self.client().post('/api/questions', json=self.new_question)
+        data = json.loads(res.data)
 
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertTrue(data['created'])
-    #     self.assertTrue(len(data['questions']))
-    #     self.assertTrue(data['total_questions'])
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
 
-    # def test_create_new_questions_fails_with_422(self):
-    #     res = self.client().post('/api/questions', json=self.new_faulty_question)
-    #     data = json.loads(res.data)
+    def test_create_new_questions_fails_with_422(self):
+        res = self.client().post('/api/questions', json=self.new_faulty_question)
+        data = json.loads(res.data)
 
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['message'], 'unprocessable')
-
-    # def test_new_search(self):
-    #     res = self.client().post('/api/search', json={
-    #         'searchTerm': 'What'
-    #     })
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertTrue(len(data['questions']))
-    #     self.assertTrue(data['total_questions'])
-
-    # def test_new_search_has_no_result(self):
-    #     res = self.client().post('/api/search', json={
-    #         'searchTerm': 'eofijefsof'
-    #     })
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertEqual(data['questions'], None)
-    #     self.assertEqual(data['total_questions'], 0)
-
-    # def test_get_questions_by_category(self):
-    #     res = self.client().get('/api/categories/1/questions')
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     # self.assertTrue(data['total_questions'])
-    #     # self.assertTrue(len(data['questions']))
-    #     # self.assertTrue(data['current_category'])
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
 
 # Make the tests conveniently executable
